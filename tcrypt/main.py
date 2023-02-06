5f0c18137cecd6f934801c0b266dd8c11d6bc413
Cesar Gaspar <rasec.rapsag@gmail.com>
2023-02-06 18:16:32 -0300

    Updated main app

--- tcrypt/main.py
+++ tcrypt/main.py
@@ -1,6 +1,16 @@
+from truecrypt import Truecrypt
 from tcrypt import TCrypt
 
 
 if __name__ == '__main__':
     tcrypt = TCrypt()
-    
+    true = Truecrypt()
+
+    if tcrypt.args.init:
+        print('Initializing truecrypt container...')
+        if not true.start(tcrypt.args.init):
+            tcrypt.error(true.error)
+    elif tcrypt.args.stop:
+        print('Stopping truecrypt container...')
+        if not true.stop():
+            tcrypt.error(true.error)
